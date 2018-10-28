2a49750d4e51de3ab7be4f1fbd06563ca8ccdf92
Jonas Gessner <JonasGessner@users.noreply.github.com>
2018-10-28 18:06:33 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Swifter",
-    swiftLanguageVersions: [.v4_2],
     targets: [
         .target(
             name: "Swifter"
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
