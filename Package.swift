e17a6b11f88571f5cc57d31216a4ecfcdbc2f755
Jonas Gessner <JonasGessner@users.noreply.github.com>
2018-10-28 18:15:28 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Swifter",
+    products: [
+        .library(name: "Swifter", targets: ["Swifter"]),
+    ],
     targets: [
         .target(
             name: "Swifter",
